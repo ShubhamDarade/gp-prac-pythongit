70d6239d705eb1094471c155a1cd67567ebf3b44
ShubhamDarade <shubhamdarade717@gmail.com>
2024-06-17 14:05:36 +0530

    Added Input parameters

--- rectangle.py
+++ rectangle.py
@@ -5,3 +5,10 @@ def perimeter(length, breadth):
 def area(length,bredth):
     a=length*breadth
     return a
+
+l=int(input('Enter the length : '))
+b=int(input('Enter the breadth : '))
+
+print(perimeter(l,b))
+print(area(l,b))
+
